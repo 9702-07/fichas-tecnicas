cb1c8b95ac0cc43fca243cf1bfd23b001f79e11b
9702-07 <alexsangarc9702@gmail.com>
2026-06-02 13:08:24 -0500

    usar tmp para uploads y output

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.secret_key = 'pacific-control-fichas-2026'
 
 BASE_DIR    = Path(__file__).parent
-UPLOAD_DIR  = BASE_DIR / 'uploads'
-OUTPUT_DIR  = BASE_DIR / 'output'
+UPLOAD_DIR  = Path("/tmp/uploads")
+OUTPUT_DIR  = Path("/tmp/output")
 TEMPLATE_PDF = BASE_DIR / 'Modelo ficha tecnica.pdf'
 
 UPLOAD_DIR.mkdir(exist_ok=True)
